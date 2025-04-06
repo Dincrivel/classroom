49e8a11b6b1a93d66efd2ea99ec729a91be3b2be
diego <diego.velasques@outlook.com>
2025-04-05 23:15:04 -0300

    Integration of lesson01, lesson02, lesson03 to the directory Classroom.

---
+++ lesson01.js
@@ -0,0 +1,9 @@
+//output
+console.log("Type your nickname")
+
+//variable declaration
+    let nickname = "Dincrivel"
+
+//concatenate a fixed mensage + nickname variable
+console.log("Welcome, " + nickname + "!")
+console.log(nickname + " just arrived!")
---
+++ lesson02.js
@@ -0,0 +1,9 @@
+//declaring a constant
+const popup = "The game says: "
+
+//concatenating string const + string message
+console.log(popup + "A new enemy appeared!")
+console.log(popup + "You have been defeated.")
+
+popup = "The game2"
+console.log("Try the new game " + popup + "!!")
