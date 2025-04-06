cf519b3f8b7d5deee82674eeeff661b2ef899581
diego <diego.velasques@outlook.com>
2025-04-06 09:26:22 -0300

    adding lesson05: fixing variables typres + mini exercise w/ keyboard

---
+++ lesson05.js
@@ -0,0 +1,19 @@
+//name, age, phone number, id, adress, social care
+// string = text
+// number = numbers
+// boolean = logic values
+
+let personName = "Eloisa do Javascript"
+let age = 78
+let phoneNumber = "(11)97505-1344"
+let cpf = "123.123.321-11"
+let adress = "Boots St, 55, Strawberry Blvd"
+let socialCare = true
+
+//Exercise - Keyboard
+let keyboardName = "K500-B61W"
+let keyboardSize = 60
+let keyboardColor = "Grey"
+let keyboardLight = true
+let keyboardWifi = false
+const fn = true
