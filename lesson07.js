08e910722fec6b96140dd02d7c4f2778e48b74ad
diego <diego.velasques@outlook.com>
2025-04-06 18:22:02 -0300

    adding lesson07.js : intruduction in arithmetic operators and concatenating

---
+++ lesson07.js
@@ -0,0 +1,28 @@
+//Basic study about operators in variables and using it concatenating
+let age = 30
+console.log("my variable value: " + age)
+
+age = 30 + 6
+console.log("adiction operation: " + age)
+
+age = 30 - 7
+console.log("subtraction operation: " + age)
+
+let productCode = 1580
+let exactCode = productCode - 1000
+console.log("the product code is " + exactCode)
+
+let price = 100.99
+let priceWTaxes = price * 2
+console.log("final price is U$" + priceWTaxes)
+
+let multiplier = 4
+let multiplying = 12
+let product = multiplier * multiplying
+console.log("final result is " + product)
+
+let marketNote = 50
+console.log("division operation " + marketNote / 2)
+
+let calculus = 10 % 3
+console.log("the change is: " + calculus)
