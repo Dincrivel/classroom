19effef5309a7df40e3ce18d5d6de7ebcb86282e
diego <diego.velasques@outlook.com>
2025-04-06 09:08:16 -0300

    adding lesson04 : pokemon scheme to explain types of variables on JS

---
+++ lesson04.js
@@ -0,0 +1,8 @@
+//pokemon scheme
+let pokemonName = "Ceruledge"   //String (characters)
+let pokemonSex = 'F'
+
+let pokemonLevel = 43           //Number (numbers...)
+let pokemonHP = 55
+
+let pokemonEnable = true        //Boolean (true or false)
