804cc646af1a916f38dd4dd9e003ca67272b5b3d
diego <diego.velasques@outlook.com>
2025-04-07 09:17:43 -0300

    update lesson07.js : completing the operators course

--- lesson07.js
+++ lesson07.js
@@ -39,11 +39,61 @@ timer--
 timer--
 console.log(timer)
 
-let price = 10
+price = 10
 price += 5 //price = price + 5
 price -= 10 //price = price - 10
 price *= 10 //price = price * 10
-console.log(price)
+console.log("U$" + price)
 
 let shopping = 2 * ((5 + 3) - 10) //setting priorites into the operation
 console.log(shopping)
+
+let number = "1"
+let label = "AMD"
+let result = label !== "Intel" // stock the result (true?false) in a variable and show it after this
+console.log(number == 1) //is number = 1? <true>
+console.log(label !== "AMD")
+console.log(label === "AMD")
+console.log(result)
+
+let cpfBlocked = "123.445.222-45"
+let cpfUser = "222.111.222=09"
+let isCPFBlocked = cpfUser === cpfBlocked //it is equal?
+console.log("Can the user blocked? " + isCPFBlocked)
+// = is atribuition
+// == is to compare values
+// === is to compare value AND format of content
+// !== is this diferent?
+
+
+let cpfPermit = "321.332.555-10"
+cpfUser = "321.332.555-11"
+let cpfVerify = cpfPermit !== cpfUser //is not equal
+console.log("The user is invalid? " + cpfVerify)
+
+//Comparing now with aritmethic values
+let ageMin = 18
+let userAge = 18
+let ageVerify = userAge >= ageMin // >= (user equal or greater than min)
+console.log("User has the minumun age? " + ageVerify)
+
+number = 5
+string = "5"
+let verifyNumber = number === string
+console.log("is it a REAL number? " + verifyNumber) // === is to compare value AND format of content
+
+// AND (&&), OR (||) and NOT (!) logical operations
+
+age = 22
+let passportVisa = true
+console.log( (age >= 18) && (passportVisa === true) )//both conditions need to match to return a (true) result
+
+let weather = "clean"
+let item = "umbrella"
+let canLeave = (weather !== "rain") || (item === "umbrella")//one of conditions must be (true) to return (true)
+console.log("nosso personagem pode sair? " + canLeave)
+
+weather = "rain"
+let hour = 8
+result = (weather !== "rain") && (hour > 6)//the condition must be (false) to return a (true) result. you can change the boolean result by putting (!) in front of it
+console.log(!result)//!xxxxx the result changes to (ture) or (false)
