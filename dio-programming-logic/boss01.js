4c93302b280ea2c677c5c63c61e420e3ff1e69d7
diego <diego.velasques@outlook.com>
2025-04-13 23:09:23 -0300

    update : boss01.js cleaning the code committed

--- dio-programming-logic/boss01.js
+++ dio-programming-logic/boss01.js
@@ -1,7 +1,6 @@
 // HERO LEVEL CLASSIFIER
 // Create a variable to store the hero's name and experience points (XP), then use a decision structure to display one of the messages below:
 
-//  
 // If XP is less than 1,000 = Iron  
 // If XP is between 1,001 and 2,000 = Bronze  
 // If XP is between 2,001 and 5,000 = Silver Gold  
