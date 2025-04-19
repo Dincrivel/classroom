4d9ce86fa1b906e7c1835169da24aef2d58b6e98
diego <diego.velasques@outlook.com>
2025-04-19 20:34:38 -0300

    adding : boss03.js - implementing a generic hero class with attack method and properties

---
+++ dio-programming-logic/boss03.js
@@ -0,0 +1,58 @@
+// ## Objetivo:
+// 
+// Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+// 
+// - nome
+// - idade
+// - tipo (ex: guerreiro, mago, monge, ninja )
+// 
+// além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+// 
+// - exibir a mensagem: "o {tipo} atacou usando {ataque}")
+// - aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+// - e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+// 
+// se mago -> no ataque exibir (usou magia)
+// se guerreiro -> no ataque exibir (usou espada)
+// se monge -> no ataque exibir (usou artes marciais)
+// se ninja -> no ataque exibir (usou shuriken)
+// 
+// ## Saída
+// 
+// Ao final deve se exibir uma mensagem:
+// 
+// - "o {tipo} atacou usando {ataque}"
+//   ex: mago atacou usando magia
+//   guerreiro atacou usando espada
+
+class hero{
+    constructor(name, age, type, move){
+        this.name = name
+        this.age = age
+        this.type = type
+        this.move = move
+
+        switch (type) {
+            case "ninja":
+                this.move = "shuriken"
+                break
+            case "mage":
+                this.move = "magic"
+                break
+            case "warrior":
+                this.move = "sword"
+                break
+            case "monk":
+                this.move = "martial arts"
+                break
+            default:
+                this.move = "unknown attack"
+        }
+    }
+
+    attack(){
+        console.log(`the ${this.type} attacked using ${this.move}`)
+    }
+}
+let ninjaHero = new hero("Diego", 26, "ninja")
+ninjaHero.attack()
